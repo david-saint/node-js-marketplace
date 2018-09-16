003c2bcba8119e6ee49127ad14fd735bd9f6b35d
David Saint <topstronics@gmail.com>
2018-09-16 21:38:02 +0100

    Can update and find stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -20,6 +20,29 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 };
 
-exports.getStores = (req, res) => {
-  res.render('stores', { title: 'Stores' });
+exports.getStores = async (req, res) => {
+  // 1.Query the database for a list of stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find store with given id
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // 3. Reder out the edit form for so the user can update the info
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find the store with the given id
+  // 2. Update and SAve the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old store
+    runValidators: true, // run the required validators
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>.
+    <a href="/stores/${store.slug}">View Store ⏭</a>`);
+  // 3. Redirect to
+  res.redirect(`/stores/${store._id}/edit`);
 };
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,9 @@ const router = express.Router();
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.get('/stores', storeController.getStores);
 router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
